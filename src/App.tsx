a8a1cbb4eee9ff02b6190574f0339fd3cbebba1c
leoarmelin <leonardoarmelin@codeby.com.br>
2021-08-07 12:59:08 -0300

    feat(global): copy files from example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+import React, { useState } from 'react';
+
+import { Typography } from 'react-easy-typography';
+
+import { Container, ExamplesContainer, Button } from './styles/App';
+import GlobalStyle from './styles/global';
+
+import { screenToggler, ScreensList } from './utils/screenToggler';
+
+const App = () => {
+  const [screen, toggleScreen] = useState<keyof ScreensList | undefined>(
+    undefined
   );
-}
+  return (
+    <Container>
+      <GlobalStyle />
+      <Typography type='TitleOne' className='title'>
+        Welcome to react-easy-typography!
+      </Typography>
+      <Typography type='TitleThree'>
+        Choose one of the examples to run:
+      </Typography>
+
+      <ExamplesContainer>
+        <Button onClick={() => toggleScreen('classnameScreen')}>
+          <Typography>Change css with classname</Typography>
+        </Button>
+        <Button onClick={() => toggleScreen('styledScreen')}>
+          <Typography>Change css with styled components</Typography>
+        </Button>
+      </ExamplesContainer>
+
+      {screen && screenToggler(screen)}
+    </Container>
+  );
+};
 
 export default App;
